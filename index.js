5ab8a4e37cbb96e43586540fe0653468d84e5789
fatima Muhammad Yousuf <its_fatimamuhammadyousuf@gmail.com>
2024-09-16 18:09:42 +0500

    function

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var a = 5;
 console.log(a); // This will correctly log the value of 'a'
+var title;
+title = "Shah rahe faisal";
+console.log(title);
 //type annoatation:
-var fullName = "fsy";
-fullName = 'john';
+var fullName;
+fullName = 'fsy';
+console.log(fullName);
 //declare it
 var number;
 //initialize it
 number = 123;
+// print
+console.log(number);
 //string array only string intake
 var names = [];
 names.push("fatima");
 names.push("fsy");
 names.push("123");
+names.pop();
 console.log(names);
+names.pop();
 var age;
 age = 23;
 var ages = [];
@@ -31,8 +41,50 @@ person = { name: 'fsy' };
 var family;
 family = { name: 'huda' };
 family = { name: 'hudabakht', age: 30 };
+var shah_Family_tree;
+shah_Family_tree = { name: 'aliabhutt', school: 'yangi' };
+shah_Family_tree = { name: 'honda', age: 30, school: "softwere-Engineer" };
+shah_Family_tree = { name: 'faqa', age: 78, school: 'abc', hobby: "cricket" };
+console.log(shah_Family_tree);
 function divide(a, b) {
     return a / b;
 }
 var familier;
 familier = [13, "haji", 122];
+// type annonatation
+var fulhouse = "fatii";
+var num;
+num = 1;
+var subjects = [];
+subjects.push("chemistry");
+//functions
+function family_tree(a, b) {
+    return a + b;
+}
+function family_history(a, b) {
+    return a;
+}
+// TUPLES 
+var address_place;
+address_place = [222, "shaikh shaheen road", 9898];
+//objects
+var person_name;
+person_name = { name: 'her name is shah' };
+// decimal leading to zeros are not allowed
+var person_identity;
+person_identity = { identity: 1923 };
+//optional value 
+function add(a, b) {
+    return a + b;
+}
+console.log(add('abc', 'sde'));
+// let nationality : 
+var nation;
+nation = { names_of_nation: "pakistan", no_of_people: 2000 };
+nation = { names_of_nation: "india" };
+nation = { names_of_nation: "Americans", no_of_people: 100000000000000000000 };
+console.log(nation);
+function find_home(street, address) {
+    return street + address;
+}
+console.log(add("sheikh Shaheen Road Sukkur", "hyderabad"));
--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
+import { createModuleResolutionCache } from "typescript";
+
 let a: number = 5;
 console.log(a);  // This will correctly log the value of 'a'
 
+
+let title: string;
+title="Shah rahe faisal";
+console.log(title);
 //type annoatation:
 
-let fullName : string = "fsy";
-
-fullName = 'john';
+let fullName : string;
+fullName = 'fsy';
+console.log(fullName);
 
 //declare it
 let number : number;
@@ -13,12 +19,17 @@ let number : number;
 //initialize it
 number  = 123; 
 
+// print
+console.log(number);
+
 //string array only string intake
 const names : string[] = [];
 names.push("fatima");
 names.push("fsy");
 names.push("123");
+names.pop();
 console.log(names);
+names.pop();
 
 let age : number;
  age = 23;
@@ -45,6 +56,18 @@ let family : {name: string, age?: number};
 family = {name: 'huda'}
 family = {name : 'hudabakht', age: 30};
 
+let shah_Family_tree : {
+   name: string, 
+   age?: number, 
+   school: string, 
+   hobby?: string
+};
+shah_Family_tree={name: 'aliabhutt',school: 'yangi'}
+shah_Family_tree={name: 'honda', age:30, school:"softwere-Engineer" };
+shah_Family_tree= {name: 'faqa', age:78,school:'abc',hobby : "cricket"};
+
+console.log(shah_Family_tree);
+
 function divide(a:number, b:number) :number{
    return a/b;
 
@@ -62,6 +85,7 @@ num =1;
 const subjects: string[] = [];
 subjects.push("chemistry");
 
+//functions
 function family_tree(a:number, b:number) : number{
    return a+b;
 }  
@@ -70,4 +94,38 @@ function family_tree(a:number, b:number) : number{
 function family_history(a:string, b:string) : string {
     return a;
     
+} 
+// TUPLES 
+let address_place : [number, string, number];
+address_place =[ 222, "shaikh shaheen road", 9898]
+
+//objects
+
+let person_name : {name : string};
+person_name = { name: 'her name is shah'};
+
+
+// decimal leading to zeros are not allowed
+let person_identity :{ identity : number};
+person_identity = { identity: 1923 }; 
+
+//optional value 
+function add (a:string, b?:string){
+return a+b;
 }
+
+console.log(add('abc', 'sde'));
+
+// let nationality : 
+let nation: {names_of_nation: string, no_of_people?: number};
+nation= {names_of_nation: "pakistan", no_of_people: 2000};
+nation ={names_of_nation: "india"};
+nation = {names_of_nation: "Americans", no_of_people: 100000000000000000000};
+console.log(nation);
+
+
+function find_home(street:string, address: string){
+   return street+address;
+}
+
+console.log(add("sheikh Shaheen Road Sukkur","hyderabad"));
