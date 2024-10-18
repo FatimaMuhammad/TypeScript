6a7e7de4c45acf21c2ace58f97f51cc1df4cd986
fatima Muhammad Yousuf <its_fatimamuhammadyousuf@gmail.com>
2024-10-18 12:12:02 +0500

    datatypes done

---
+++ 02_varibles.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all js in new version
+// alert (3+3) I am using  nodejs , not browser
+// avoid semicolon;
+
+// readbale + 
+console.log(3+3);
+console.log("Fatima Muhammad")
+
+// data Types
+ let name = "fatima";
+ let age = 90;
+ let isLoggedIn = false;
+ 
+// primitive data types
+ // number => 2 to power 53
+ //bigint
+ //string '', "",
+ //boolean => true /false
+ // null => stand alone value; // empty // value define ni hui h 
+ // undefined => not defineing any value
+ // nul is not undefined it is empty
+ // symbol => to find uniqueness  
+
+ // object
+ console.log(typeof null);
+ console.log(typeof undefined);
+ //null is an object 
